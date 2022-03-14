bb3381cf2eae68d0b6feb14264007917fbaa6a46
Jdogzz <jerzor@pacbell.net>
2022-03-13 22:37:23 -0700

    Fixed relative launch path

--- sfbib.el
+++ sfbib.el
@@ -3,9 +3,11 @@
   (interactive "sEnter your URL: ")
   (defvar strippedurl (shell-command-to-string (format "echo -n %s | sed -e 's/[^A-Za-z0-9._-]/_/g'" x)))
   (defvar thetimenow (format-time-string "%Y%m%d%H%M%S"))
-  (write-region (concat "\n@online{" thetimenow strippedurl ",\n  title = {" (shell-command-to-string (format "%s %s %s" "./sfcaller.sh" x thetimenow)) "},\n  url = {" x "},\n  file={" singlefile-destination-dir thetimenow strippedurl "},\n}") nil (car bibtex-completion-bibliography) 'append)
+  (write-region (concat "\n@online{" thetimenow strippedurl ",\n  title = {" (shell-command-to-string (format "%s %s %s" (expand-file-name "./sfcaller.sh" directory-of-sfbibel) x thetimenow)) "},\n  url = {" x "},\n  file={" singlefile-destination-dir thetimenow strippedurl "},\n}\n") nil (car bibtex-completion-bibliography) 'append)
   )
 
 (defcustom singlefile-destination-dir "/mydata/references/webpages/"
   "Files downloaded by single-file go here."
   :type 'string)
+
+(defconst directory-of-sfbibel (file-name-directory load-file-name))
